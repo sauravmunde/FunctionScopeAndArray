12b5976aeaa528b46c3e505e52a187258e75bac9
sauravmunde <souravmunde2001@gmail.com>
2025-07-05 11:17:07 +0530

    first commit

---
+++ functionCallingWorks.java
@@ -0,0 +1,21 @@
+public class functionCallingWorks {
+
+    public static int sum(int a, int b) {
+
+        int result = a + b;
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+
+        int b = 5;
+
+        int result = sum(a, b);
+
+        System.out.println(result);
+    }
+
+}
