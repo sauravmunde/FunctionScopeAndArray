12fc0be670c602ff487afc42913f751328ff8271
sauravmunde <souravmunde2001@gmail.com>
2025-07-05 09:56:41 +0530

    first commit

---
+++ firstCode.java
@@ -0,0 +1,17 @@
+public class firstCode {
+
+    public static int addnum(int a, int b) {
+        int c = a + b;
+        return c;
+
+    }
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println(addnum(a, b));
+    }
+
+}
