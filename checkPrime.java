61d4f2d74154cd8b2a9faa6e23342a5c061a8fef
sauravmunde <souravmunde2001@gmail.com>
2025-07-05 16:55:31 +0530

    first commit

---
+++ checkPrime.java
@@ -0,0 +1,16 @@
+public class checkPrime {
+
+    public static boolean isPrime(int x) {
+        for (int i = 2; i < x / 2; i++) {
+            if (x % i == 0)
+                return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print(isPrime(47));
+    }
+
+}
