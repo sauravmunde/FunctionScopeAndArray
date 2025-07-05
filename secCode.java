637d64efb915e528b12699bd95203ac029bfe9fc
sauravmunde <souravmunde2001@gmail.com>
2025-07-05 10:06:04 +0530

    first commit

---
+++ secCode.java
@@ -0,0 +1,14 @@
+public class secCode {
+
+    public static double add(int a, int b) {
+        float c = a + b;
+        System.out.println(c);
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 3));
+    }
+
+}
