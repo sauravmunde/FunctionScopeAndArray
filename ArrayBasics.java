8a693979d9a2fb9d63e6ad420e0eb3e651712a03
sauravmunde <souravmunde2001@gmail.com>
2025-07-08 11:39:56 +0530

    first commit

---
+++ ArrayBasics.java
@@ -0,0 +1,33 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+        int arr[] = new int[20];
+
+        arr[0] = 74;
+
+        System.out.println(arr[0] = 11);
+
+        arr[0] = 400;
+
+        System.out.println(arr[0]);
+
+        char carray[] = new char[-10];
+        System.out.println(carray);
+        double dArray[] = new double[10];
+
+        System.out.println(carray[0]);
+        System.out.println(dArray[0]);
+
+        int a[] = new int[100];
+
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+
+        System.out.println(a[0] + a[1] + a[2] + a[3] + a[4]);
+
+    }
+
+}
---
+++ arrayOperations.java
@@ -0,0 +1,32 @@
+public class arrayOperations {
+    public static void main(String[] args) {
+
+        // this is the code for arr
+
+        int arr[] = new int[5];
+
+        System.out.println(arr[0] = 1);
+
+        // char type of the code
+
+        char chArray[] = new char[15];
+
+        System.out.println(chArray);
+
+        System.out.println();
+
+        // boolean type of the array
+
+        boolean arr1[] = new boolean[5];
+
+        System.out.println(arr1[5]);
+
+        //
+        int arr2[] = new int[5];
+        arr2[1] = 10;
+        arr2 = new int[2];
+        System.out.println(arr2[1]);
+
+    }
+
+}
